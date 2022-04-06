0ec2d990cde5afe5ac47c5c690a1e220d26fcb42
udhaygithub <98750100+udhaygithub@users.noreply.github.com>
2022-04-06 10:40:22 +0100

    Add files via upload

---
+++ scripts/jsAction_644ebc43-4e67-421f-8867-9640fbc2de50.js
@@ -0,0 +1,36 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var IterationNumber = context.variableManager.getValue("IterationVal2.Values");
+context.variableManager.setValue("Iterationval",IterationNumber);
+var IterationNumber1=parseInt(IterationNumber);
+if (IterationNumber1%2==0) 
+{
+      var CondCheck=2;
+      context.variableManager.setValue("OnceIntwo",CondCheck);
+}
+else
+{
+    var CondCheck=1;
+      context.variableManager.setValue("OnceIntwo",CondCheck);
+}
+if (IterationNumber1%3==0) 
+{
+      var CondCheck=3;
+      context.variableManager.setValue("EventVar",CondCheck);
+   
+}
+else if (IterationNumber1%2==0) 
+{
+    var CondCheck=2;
+      context.variableManager.setValue("EventVar",CondCheck);
+   
+}
+else
+{
+     var CondCheck=1;
+      context.variableManager.setValue("EventVar",CondCheck);
+}
---
+++ scripts/jsAction_ec5004da-e4f5-43d3-a5c5-2b0728ff38cb.js
@@ -0,0 +1,31 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var IterationNumber = context.variableManager.getValue("Iterationval");
+var IterationNumber1=parseInt(IterationNumber);
+if (IterationNumber1%4==0) 
+{
+      var CondCheck=4;
+      context.variableManager.setValue("OnceInFour",CondCheck);
+}
+
+else if (IterationNumber1%3==0) 
+{
+      var CondCheck=3;
+      context.variableManager.setValue("OnceInFour",CondCheck);
+    
+}
+else if (IterationNumber1%2==0) 
+{
+    var CondCheck=2;
+      context.variableManager.setValue("OnceInFour",CondCheck);
+    
+}
+else
+{
+     var CondCheck=1;
+      context.variableManager.setValue("OnceInFour",CondCheck);
+}
